7f49005471955aba3eece392768ede81b2e5d8bf
“jameswatt” <jameswatt2008@gmail.com>
2016-01-25 17:49:01 +0800

    Initial Commit

---
+++ 6-GCD2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  6-GCD2
+//
+//  Created by jameswatt on 16/1/25.
+//  Copyright © 2016年 xuzhixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 6-GCD2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  6-GCD2
+//
+//  Created by jameswatt on 16/1/25.
+//  Copyright © 2016年 xuzhixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
